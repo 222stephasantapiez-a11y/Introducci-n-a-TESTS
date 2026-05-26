7313326b9dc6aaf850cbfaf47b6d9dd6d5d7dc71
stephanie <222stephasantapiez@gmail.com>
2026-05-26 07:19:27 -0500

    se agrego las dos operacion en ejemplo 2 en el archivo mathHelper

--- ejemplo 2/src/mathHelper.js
+++ ejemplo 2/src/mathHelper.js
@@ -2,9 +2,17 @@ function add(a, b) {
   return a + b;
 }
 
+function restar(a, b) {
+  return a - b;
+}
+
+function multiplicacion(a, b){
+  return a * b;
+}
+
 function divide(a, b) {
   if (b === 0) throw new Error('No se puede dividir entre cero.');
   return a / b;
 }
 
-module.exports = { add, divide };
+module.exports = { add, divide, restar, multiplicacion };
